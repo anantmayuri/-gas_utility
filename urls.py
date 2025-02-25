d0539e59caf17520516cdba6e09aa33177d9ae5b
Mayuri Anant <77527747+anantmayuri@users.noreply.github.com>
2025-02-25 11:17:07 +0530

    urls.py

--- urls.py
+++ urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from service_requests.views import homepage  # Import the homepage view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('requests/', include('service_requests.urls')),
+     path('', homepage, name='homepage'),  # Redirect homepage to the request submission page
 ]
+
